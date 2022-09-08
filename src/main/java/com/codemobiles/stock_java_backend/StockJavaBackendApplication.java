df9f5139247ac240a532993f2f9a3906176616ea
natdanai <natdanai.thaiyala@gmail.com>
2022-09-08 13:33:41 +0700

    Update
    How to use @Bean and CommandLineRunner

--- src/main/java/com/codemobiles/stock_java_backend/StockJavaBackendApplication.java
+++ src/main/java/com/codemobiles/stock_java_backend/StockJavaBackendApplication.java
@@ -2,9 +2,11 @@ package com.codemobiles.stock_java_backend;
 
 import java.util.Arrays;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 //@SpringBootApplication
 @EnableAutoConfiguration
@@ -19,6 +21,12 @@ public class StockJavaBackendApplication {
 		for(String beanName : beanNames) {
 			System.out.println(beanName);
 		}
+	}
+	
+	@Bean
+	CommandLineRunner init() {
+		System.out.println("natdanai");
+		return null;
 	}
 
 }
