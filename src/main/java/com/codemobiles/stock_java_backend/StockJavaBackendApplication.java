a4003a20517344774f55cd6d7493777506539670
natdanai <natdanai.thaiyala@gmail.com>
2022-09-08 11:25:18 +0700

    new project

---
+++ src/main/java/com/codemobiles/stock_java_backend/StockJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codemobiles.stock_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemobiles/stock_java_backend/StockJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemobiles.stock_java_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
