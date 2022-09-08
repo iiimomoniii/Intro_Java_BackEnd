0a42bc7a34b0882b301ad4b2d39c616c8a5a133d
natdanai <natdanai.thaiyala@gmail.com>
2022-09-08 15:21:15 +0700

    Update
    create RestController and return dateUtils by dependency injection on spring boot framwork

--- src/main/java/com/codemobiles/stock_java_backend/controller/DemoController.java
+++ src/main/java/com/codemobiles/stock_java_backend/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.codemobiles.stock_java_backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import com.codemobiles.stock_java_backend.util.DateUtils;
 @RestController
 public class DemoController {
 
+	@Autowired
+	DateUtils dateUtils;
+	
 	@GetMapping("/")
-	String getToday(DateUtils dateUtils) {
+	String getToday() {
 		return dateUtils.todayString();
 	}
 	
