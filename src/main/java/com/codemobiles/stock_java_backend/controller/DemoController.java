832696ef1f52a03515b276b90577fca8971e6744
natdanai <natdanai.thaiyala@gmail.com>
2022-09-09 15:03:09 +0700

    Update
    fix multiple bean when implements in service

--- src/main/java/com/codemobiles/stock_java_backend/controller/DemoController.java
+++ src/main/java/com/codemobiles/stock_java_backend/controller/DemoController.java
@@ -1,6 +1,6 @@
 package com.codemobiles.stock_java_backend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +9,46 @@ import com.codemobiles.stock_java_backend.util.DateUtils;
 @RestController
 public class DemoController {
 
-	@Autowired
 	DateUtils dateUtils;
+	private SayService sayService;
+	
+	//การเเก้ไข multiple bean ใน SayService โดยการเรียก ชื่อ component โดยหลังจากเรียกเเล้วมันก็จะเอาไปลงทะเบียน ใน spring boot ให้
+	//เช่น dog
+	DemoController(DateUtils dateUtils, SayService dog){
+		this.dateUtils = dateUtils;
+		this.sayService = dog;
+	}
 	
 	@GetMapping("/")
 	String getToday() {
 		return dateUtils.todayString();
 	}
 	
+	@GetMapping("/say")
+	String say() {
+		return sayService.say();
+	}
+	
+}
+
+interface SayService {
+	String say();
+}
+
+//register Cat
+@Component
+class Cat implements SayService {
+	@Override
+	public String say() {
+		return "meooow";
+	}
+}
+
+//register Dog
+@Component
+class Dog implements SayService {
+	@Override
+	public String say() {
+		return "hoooog";
+	}
 }
