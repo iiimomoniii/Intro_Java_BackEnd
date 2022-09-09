d7ed6b382e58d29f54b604e03f3e782835c90cb0
natdanai <natdanai.thaiyala@gmail.com>
2022-09-09 14:27:28 +0700

    Update DemoController.java

--- src/main/java/com/codemobiles/stock_java_backend/controller/DemoController.java
+++ src/main/java/com/codemobiles/stock_java_backend/controller/DemoController.java
@@ -1,6 +1,5 @@
 package com.codemobiles.stock_java_backend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +8,16 @@ import com.codemobiles.stock_java_backend.util.DateUtils;
 @RestController
 public class DemoController {
 
-	@Autowired
 	DateUtils dateUtils;
+	
+	DemoController(DateUtils dateUtils){
+		this.dateUtils = dateUtils;
+	}
 	
 	@GetMapping("/")
 	String getToday() {
+		//เอา @Autowired ออกเพราะตอนทำ unit test เพราะ constructor มัน force ให้ส่งตัวเเปรเข้ามา ตอน new object ขึินมาใหม่มัน require parameter เพื่อให้เราไม่ลืม mock data เพื่อตรวจสอบ
+		DemoController demo = new DemoController(null);
 		return dateUtils.todayString();
 	}
 	
