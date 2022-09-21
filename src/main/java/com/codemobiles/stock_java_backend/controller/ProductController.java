8d3754f8e4d0901e8a6922482320736c328fb4a8
iiimomoniii <natdanai_thaiyala@live.com>
2022-09-21 15:06:20 +0700

    Update
    create new path for getProducts and getProduct by ID

--- src/main/java/com/codemobiles/stock_java_backend/controller/ProductController.java
+++ src/main/java/com/codemobiles/stock_java_backend/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.codemobiles.stock_java_backend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
 	//request -> path -> process -> response
-	@RequestMapping("/say")
-	public String say() {
-		return "Hi";
+	@GetMapping("/say")
+	public String getProducts() {
+		return "Get Product All";
+	}
+	
+	@GetMapping("/say/{id}")
+	public String getProduct() {
+		return "Get Product By ID";
 	}
 }
