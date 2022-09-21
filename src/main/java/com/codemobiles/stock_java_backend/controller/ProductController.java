5ced9b4d2ef23de29a483bb83844997c44104a26
iiimomoniii <natdanai_thaiyala@live.com>
2022-09-21 15:14:23 +0700

    Update
    change method name and show id in message

--- src/main/java/com/codemobiles/stock_java_backend/controller/ProductController.java
+++ src/main/java/com/codemobiles/stock_java_backend/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.codemobiles.stock_java_backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
 	//request -> path -> process -> response
-	@GetMapping("/say")
+	@GetMapping("/getProducts")
 	public String getProducts() {
 		return "Get Product All";
 	}
 	
-	@GetMapping("/say/{id}")
-	public String getProduct() {
-		return "Get Product By ID";
+	@GetMapping("/getProduct/{id}")
+	public String getProduct(@PathVariable long id) {
+		return "Get Product By ID : " + id;
 	}
 }
