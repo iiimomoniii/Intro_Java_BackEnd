fbae98c9efd422682f7eb3ee45b39003bb701a1b
iiimomoniii <natdanai_thaiyala@live.com>
2022-09-21 14:56:57 +0700

    Update
    ใช้ @RestController แทน @Controller โดยไม่ต้องกังวลเรื่องลืมใส่ @ResponseBody

--- src/main/java/com/codemobiles/stock_java_backend/controller/ProductController.java
+++ src/main/java/com/codemobiles/stock_java_backend/controller/ProductController.java
@@ -3,13 +3,13 @@ package com.codemobiles.stock_java_backend.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ProductController {
 
 	//request -> path -> process -> response
 	@RequestMapping("/say")
-	@ResponseBody
 	public String say() {
 		return "Hi";
 	}
