ab3608377e30ce1bcc1b308a957cacedec92cac2
iiimomoniii <natdanai_thaiyala@live.com>
2022-09-26 11:28:45 +0700

    Update ProductController.java
    การ get query โดยที่มีการใช้ค่าที่ส่งมากับตัวเเปร name มาเเสดง

--- src/main/java/com/codemobiles/stock_java_backend/controller/ProductController.java
+++ src/main/java/com/codemobiles/stock_java_backend/controller/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +30,10 @@ public class ProductController {
 	@GetMapping({"/getProduct/id/{id}/name/{name}","/getProduct/id/{id}"})
 	public String getProductByIdAndName(@PathVariable(value="id") long id,  @PathVariable(required = false) String name) {
 		return "Get Product By ID : " + id + " Name : " + name;
+	}
+	
+	@GetMapping({"/getProduct/name"})
+	public String getProductByNameQuery(@RequestParam("name") String name) {
+		return "Get Product By Name : " + name;
 	}
 }
