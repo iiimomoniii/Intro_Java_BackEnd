da4c639e4a7079b7b83b12569cb81ec48b42f50e
iiimomoniii <natdanai_thaiyala@live.com>
2022-09-27 11:23:03 +0700

    Update
    เปลี่ยน getter setter เเละ constructor มาใช้ lombok เเทน

--- src/main/java/com/codemobiles/stock_java_backend/model/Product.java
+++ src/main/java/com/codemobiles/stock_java_backend/model/Product.java
@@ -1,51 +1,19 @@
 package com.codemobiles.stock_java_backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 	private long id;
 	private String name;
 	private String image;
 	private int price;
 	private int stock;
-	
-	public Product(long id, String name, String image, int price, int stock) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.image = image;
-		this.price = price;
-		this.stock = stock;
-	}
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getImage() {
-		return image;
-	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public int getStock() {
-		return stock;
-	}
-	public void setStock(int stock) {
-		this.stock = stock;
-	}
-	
 
 }
