8bd060011d097bc46f0c9d0dd9146597eb3a81a3
iiimomoniii <natdanai_thaiyala@live.com>
2022-09-28 15:15:32 +0700

    Update
    เพิ้ม function สำหรับ upload รูปภาพ โดยมีการกำหนดชื่อใหม่ fileName โดยใช้ UUID

--- src/main/java/com/codemobiles/stock_java_backend/service/StorageServiceImpl.java
+++ src/main/java/com/codemobiles/stock_java_backend/service/StorageServiceImpl.java
@@ -1,12 +1,16 @@
 package com.codemobiles.stock_java_backend.service;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.codemobiles.stock_java_backend.exception.StorageException;
@@ -33,7 +37,25 @@ public class StorageServiceImpl implements StorageService {
 	
 	@Override
 	public String store(MultipartFile file) {
-		return null;
+		if(file == null || file.isEmpty()) {
+			return null;
+		}
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		try {
+			//ป้องกันไม่ให้ไป upload ใน directory ก่อนหน้า
+			if (fileName.contains("..")) {
+				throw new StorageException("Path outside current directory");
+			}
+			fileName = UUID.randomUUID() + "." + fileName.substring(fileName.lastIndexOf(".") + 1);
+			
+			//return ชื่อรูปที่จะเอาไปเก็บที่ database
+			try(InputStream inputStream = file.getInputStream()){
+				Files.copy(inputStream, this.rootLocation.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+				return fileName;
+			}
+		} catch (IOException ex) {
+			throw new StorageException("Failed to store file : "+ fileName + ex.getMessage());
+		}
 	}
 
 }
